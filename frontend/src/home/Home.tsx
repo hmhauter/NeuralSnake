6830947b6bad255554194953c7cee69854789c2a
helena <helenam.hauter@gmail.com>
2026-03-08 22:12:18 +0100

    solve sesison issue

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,8 +1,18 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 import "./Home.css"
 
 export default function Home() {
   const navigate = useNavigate()
+  const [name, setName] = useState("")
+    const startGame = () => {
+    if (!name.trim()) return
+    // Carefule - usage of local storage -> will be firebase session
+    localStorage.setItem("playerName", name.trim())
+
+    navigate(`/game?player=${encodeURIComponent(name.trim())}`)
+  }
+
 
   return (
     <div className="home-container">
@@ -17,13 +27,21 @@ export default function Home() {
         <p className="landing-subtitle">
           Train AI. Play Smart. Slay the Grid.
         </p>
-
+      <div className="login-section">
+          <input
+            className="name-input"
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         <button 
           className="start-button"
-          onClick={() => navigate("/game")}
+          onClick={startGame}
         >
           Start Game →
         </button>
+        </div>
       </div>
     </div>
   )
